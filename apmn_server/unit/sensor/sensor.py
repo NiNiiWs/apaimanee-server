fea3d72e865700ca6f01d55ee08c8d8d8029a509
Aran Khunaree <new@aran>
2016-06-28 21:36:41 +0700

    create method sensor

--- apmn_server/unit/sensor/sensor.py
+++ apmn_server/unit/sensor/sensor.py
@@ -1,3 +1,15 @@
 class sensor:
-    def __init__(self):
+    def __init__(self, unit, unit_list):
+        self.unit = unit
+        self.unit_list = unit_list
+
+
+    def observer(self):
         pass
+
+    def get_range(self):
+        return unit.range
+
+    def scan(self):
+        pass
+
